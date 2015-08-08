0b0e57217bd644eade22189f5a5c309a365d7906
Kevin Grigorenko <kevin@myplaceonline.com>
2015-08-08 10:55:33 -0700

    Don't use /tmp for backup because DO has a small tmp partition

--- sh/backup_production.sh
+++ sh/backup_production.sh
@@ -7,7 +7,7 @@ echo -n "Encrypted file password: "
 read -s ENCRYPTED_FILE_PASSWORD
 echo
 read -e -p "Local output directory: " OUTPUTDIR
-OUTPUTFILE=/tmp/pgdump_myplaceonline_`date +"%Y%m%d_%H%M"`.sql
-ssh root@myplaceonline.com "PGPASSWORD=${PSQL_USER_PASSWORD} /usr/bin/pg_dump -U myplaceonline -h localhost -d myplaceonline_production > ${OUTPUTFILE} && echo '${ENCRYPTED_FILE_PASSWORD}' | /usr/bin/gpg --batch --no-tty --passphrase-fd 0 --s2k-mode 3 --s2k-count 65536 --force-mdc --cipher-algo AES256 --s2k-digest-algo sha512 -o ${OUTPUTFILE}.pgp --symmetric ${OUTPUTFILE}; rm ${OUTPUTFILE}" && \
+OUTPUTFILE=/pgdump_myplaceonline_`date +"%Y%m%d_%H%M"`.sql
+ssh root@myplaceonline.com "PGPASSWORD=${PSQL_USER_PASSWORD} /usr/bin/pg_dump -U myplaceonline -h localhost -d myplaceonline_production -Fc > ${OUTPUTFILE} && echo '${ENCRYPTED_FILE_PASSWORD}' | /usr/bin/gpg --batch --no-tty --passphrase-fd 0 --s2k-mode 3 --s2k-count 65536 --force-mdc --cipher-algo AES256 --s2k-digest-algo sha512 -o ${OUTPUTFILE}.pgp --symmetric ${OUTPUTFILE}; rm ${OUTPUTFILE}" && \
 scp root@myplaceonline.com:${OUTPUTFILE}.pgp ${OUTPUTDIR} && \
 ssh root@myplaceonline.com "rm -f ${OUTPUTFILE}.pgp"
