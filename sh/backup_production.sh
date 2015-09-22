4e8ad8d78a025bdf6904a15d2626a696a910f779
Kevin Grigorenko <kevin@myplaceonline.com>
2015-09-21 20:05:56 -0700

    Add check for backup password

--- sh/backup_production.sh
+++ sh/backup_production.sh
@@ -6,6 +6,13 @@ echo
 echo -n "Encrypted file password: "
 read -s ENCRYPTED_FILE_PASSWORD
 echo
+echo -n "Repeat Encrypted file password: "
+read -s ENCRYPTED_FILE_PASSWORD2
+echo
+if [ "${ENCRYPTED_FILE_PASSWORD}" != "${ENCRYPTED_FILE_PASSWORD2}" ]; then
+  echo "Passwords don't match! Exiting"
+  exit 1
+fi
 read -e -p "Local output directory: " OUTPUTDIR
 OUTPUTFILE=/pgdump_myplaceonline_`date +"%Y%m%d_%H%M"`.sql
 ssh root@myplaceonline.com "PGPASSWORD=${PSQL_USER_PASSWORD} /usr/bin/pg_dump -U myplaceonline -h localhost -d myplaceonline_production -Fc > ${OUTPUTFILE} && echo '${ENCRYPTED_FILE_PASSWORD}' | /usr/bin/gpg --batch --no-tty --passphrase-fd 0 --s2k-mode 3 --s2k-count 65536 --force-mdc --cipher-algo AES256 --s2k-digest-algo sha512 -o ${OUTPUTFILE}.pgp --symmetric ${OUTPUTFILE}; rm ${OUTPUTFILE}" && \
