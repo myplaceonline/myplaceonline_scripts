f8f63ed96ee409281342394c1289ba3028038aff
Kevin Grigorenko <kevin@myplaceonline.com>
2015-10-02 05:19:12 -0700

    Update monitoring script

--- sh/production_logs.sh
+++ sh/production_logs.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-LINES=10
-ssh root@myplaceonline.com "echo /var/www/html/myplaceonline/log/*; tail -$LINES /var/www/html/myplaceonline/log/*; echo /var/log/nginx/access.log; tail -$LINES /var/log/nginx/access.log; echo /var/log/nginx/error.log; tail -$LINES /var/log/nginx/error.log; echo /opt/nginx-1.7.7/access.log; tail -$LINES /opt/nginx-1.7.7/access.log; echo /opt/nginx-1.7.7/nginx_error.log; tail -$LINES /opt/nginx-1.7.7/nginx_error.log; "
+clear
+LINES=500
+ssh root@myplaceonline.com "echo /var/www/html/myplaceonline/log/*; echo "=============="; echo ""; tail -$LINES /var/www/html/myplaceonline/log/*; echo ""; echo /var/log/nginx/access.log; echo "=============="; echo ""; tail -$LINES /var/log/nginx/access.log; echo ""; echo /var/log/nginx/error.log; echo "=============="; echo ""; tail -$LINES /var/log/nginx/error.log; echo ""; echo /opt/nginx-1.7.7/access.log; echo "=============="; echo ""; tail -$LINES /opt/nginx-1.7.7/access.log; echo ""; echo /opt/nginx-1.7.7/nginx_error.log; echo "=============="; echo ""; tail -$LINES /opt/nginx-1.7.7/nginx_error.log;"
