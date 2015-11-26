402f79b89103a5d27cf4d27df747a4ced14ba973
Kevin Grigorenko <kevin@myplaceonline.com>
2015-11-26 12:48:03 -0800

    Add commit script

---
+++ sh/commit.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -x
+if [ $# -eq 0 ]; then
+  echo "Please specify commit message"
+  exit 1
+fi
+echo "$@"
+git status && \
+git add . && \
+git commit -m "$*" && \
+git push
+PARENTDIR=`dirname ${PWD}`
+while [ `basename $PARENTDIR` != "src" ]; do
+  PARENTDIR=`dirname ${PARENTDIR}`
+done
+pushd "$PARENTDIR"
+git commit -a -m "Update submodules" && \
+git push
+popd
