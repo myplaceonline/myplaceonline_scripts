73dd3160f478ba7503d1079605bf195abb7d398f
Kevin Grigorenko <kevin@myplaceonline.com>
2016-02-07 19:50:23 -0800

    Update LINES

--- sh/production_logs.sh
+++ sh/production_logs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 clear
-LINES=500
+LINES=10
 ssh root@myplaceonline.com "echo /var/www/html/myplaceonline/log/*; echo "=============="; echo ""; tail -$LINES /var/www/html/myplaceonline/log/*; echo ""; echo /var/log/nginx/access.log; echo "=============="; echo ""; tail -$LINES /var/log/nginx/access.log; echo ""; echo /var/log/nginx/error.log; echo "=============="; echo ""; tail -$LINES /var/log/nginx/error.log; echo ""; echo /opt/nginx-1.7.7/access.log; echo "=============="; echo ""; tail -$LINES /opt/nginx-1.7.7/access.log; echo ""; echo /opt/nginx-1.7.7/nginx_error.log; echo "=============="; echo ""; tail -$LINES /opt/nginx-1.7.7/nginx_error.log;"
