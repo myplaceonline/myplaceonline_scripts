8bc9d1e68c05a00ced28610867c62b6608536189
Kevin Grigorenko <kevin@myplaceonline.com>
2016-03-04 16:12:23 -0800

    Update logs script

--- sh/production_logs.sh
+++ sh/production_logs.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 clear
-LINES=10
-ssh root@myplaceonline.com "echo /var/www/html/myplaceonline/log/*; echo "=============="; echo ""; tail -$LINES /var/www/html/myplaceonline/log/*; echo ""; echo /var/log/nginx/access.log; echo "=============="; echo ""; tail -$LINES /var/log/nginx/access.log; echo ""; echo /var/log/nginx/error.log; echo "=============="; echo ""; tail -$LINES /var/log/nginx/error.log; echo ""; echo /opt/nginx-1.7.7/access.log; echo "=============="; echo ""; tail -$LINES /opt/nginx-1.7.7/access.log; echo ""; echo /opt/nginx-1.7.7/nginx_error.log; echo "=============="; echo ""; tail -$LINES /opt/nginx-1.7.7/nginx_error.log;"
+LINES=$1
+if [[ -z "$LINES" ]]; then
+  LINES=10
+fi
+ssh root@myplaceonline.com "echo ''; echo ''; echo `date`; echo `date -u`; tail -n $LINES /var/www/html/myplaceonline/log/*`date +'%Y-%m-%d'`*log; echo ''; echo /var/log/nginx/access.log; echo '=============='; echo ''; tail -$LINES /var/log/nginx/access.log; echo ''; echo /var/log/nginx/error.log; echo '=============='; echo ''; tail -$LINES /var/log/nginx/error.log; echo ''; echo /opt/nginx-1.7.7/access.log; echo '=============='; echo ''; tail -$LINES /opt/nginx-1.7.7/access.log; echo ''; echo /opt/nginx-1.7.7/nginx_error.log; echo '=============='; echo ''; tail -$LINES /opt/nginx-1.7.7/nginx_error.log;"
