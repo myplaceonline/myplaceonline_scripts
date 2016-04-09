27b25402f8877a265bdd286d3d4f82642eb8197e
Kevin Grigorenko <kevin@myplaceonline.com>
2016-04-09 10:04:33 -0700

    Change update production script

---
+++ sh/production_console.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+ssh root@myplaceonline.com "cd /var/www/html/myplaceonline/; RAILS_ENV=production bin/rails console"
+
--- sh/update_production.sh
+++ sh/update_production.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-ssh root@myplaceonline.com "/root/myplaceonline_chef/update.sh" && curl https://myplaceonline.com/
+KEYSLOADED=`ssh-add -l | grep -v "The agent has no identities." | wc -l`
+if [ $KEYSLOADED -lt 1 ]; then
+  ssh-add
+fi
+pushd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+pushd ../../myplaceonline_chefrepo
+knife ssh -C 1 "chef_environment:production AND role:web_server" "chef-client --force-logger" --ssh-user root --identity-file ~/.ssh/id_rsa
+popd
+popd
